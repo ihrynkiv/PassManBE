d3ee75d2a0a86ec2665a22d57aae290a862c77fb
Ivan Hrynkiv <Ivan.hrynkiv@gmail.com>
2022-05-01 09:55:17 +0300

    fix migrations

---
+++ migrations/1651387795471_create-users.js
@@ -0,0 +1,30 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('users', {
+    id: 'id',
+    username: {
+      type: 'varchar(100)',
+      notNull: true,
+      unique: true
+    },
+    password: {
+      type: 'varchar(256)',
+      notNull: true
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updatedAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+};
+
+exports.down = pgm => {};
