afdc1ac2ee681e4eabe3224a7cef19e75123bde0
Ivan Hrynkiv <Ivan.hrynkiv@gmail.com>
2022-05-01 08:37:42 +0300

    created userDB migration

---
+++ migrations/1651345913619_create-user-db.js
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('users', {
+    id: 'id',
+    username: {
+      type: 'varchar(100)',
+      notNull: true,
+      unique: true
+    },
+    password: {
+      type: 'varchar(256)',
+      notNull: true
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+};
+
+exports.down = pgm => {
+
+};
