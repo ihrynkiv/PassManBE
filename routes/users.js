26b09dafe0314eef2e7611d2ee9ca90367cb7648
Ivan Hrynkiv <Ivan.hrynkiv@gmail.com>
2022-05-01 09:41:02 +0300

    route define

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const usersController = require('../controllers/users.controller');
+const response = require('../middlewares/response.middleware')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',
+  usersController.getAll,
+    response.send
+  );
 
 module.exports = router;
